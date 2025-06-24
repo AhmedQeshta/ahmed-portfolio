2a5acbbcf03ef613bb07c05be5f455cce19f03bc
Ahmed Qeshta <ahmedqeshta1999@gmail.com>
2025-06-24 11:05:49 +0300

    add componets search and handel it

--- components/Search.tsx
+++ components/Search.tsx
@@ -1,26 +1,47 @@
-import Form from 'next/form';
+'use client';
+
+import { useSearch } from '@/hooks/useSearch';
+import { XIcon } from 'lucide-react';
 
 interface SearchInterface {
   action: string;
 }
 
-const Search = async ({ action }: SearchInterface) => {
-  // const { query } = await searchParams; // get that
+const Search = ({ action }: SearchInterface) => {
+  const { query, setQuery, handleSearch } = useSearch(action);
+
   return (
-    <div className="mx-auto max-w-5xl px-4 py-10">
-      <Form action={`${action}`} className="flex gap-2">
-        <input
-          placeholder="Search products"
-          type="search"
-          name="query"
-          className="flex-1 px-4 py-2 rounded-lg border border-gray-300 focus:outline-none"
-        />
+    <div className="mx-auto max-w-5xl px-4 py-5">
+      <form onSubmit={handleSearch} className="flex gap-2">
+        <div className="relative flex-1">
+          <input
+            placeholder="Search blogs..."
+            type="text"
+            name="q"
+            value={query}
+            onChange={(e) => setQuery(e.target.value)}
+            className="w-full px-4 py-2 rounded-lg border border-gray-300 focus:outline-none pr-10"
+            autoFocus
+            autoComplete="off"
+            autoCorrect="off"
+            style={{ WebkitAppearance: 'none' }}
+          />
+          {query && (
+            <button
+              type="button"
+              onClick={() => setQuery('')}
+              className="absolute text-2xl right-3 top-1/2 -translate-y-1/2 text-gray-300 hover:text-gray-400 focus:outline-none"
+              aria-label="Clear search">
+              <XIcon className="w-5 h-5" />
+            </button>
+          )}
+        </div>
         <button
           type="submit"
           className="px-4 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-600 font-bold">
-          Submit
+          Search
         </button>
-      </Form>
+      </form>
     </div>
   );
 };
