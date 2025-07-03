7c6704958e90173e339f8dd010d1a3ecb4103a2b
Ahmed Qeshta <ahmedqeshta1999@gmail.com>
2025-07-03 21:10:28 +0300

    add generateBlogMetadata on metadata and export it

--- utils/metaData.ts
+++ utils/metaData.ts
@@ -1,3 +1,6 @@
+import { sanityFetch } from '@/sanity/lib/client';
+import { blogPostBySlugQuery } from '@/sanity/lib/queries';
+import { BlogPostResponse } from '@/sanity/lib/types';
 import { Metadata, Viewport } from 'next';
 
 export const metadata: Metadata = {
@@ -56,3 +59,36 @@ export const viewport: Viewport = {
   themeColor: '#000000',
   colorScheme: 'dark',
 };
+
+// export generateMetadata for single blog
+export async function generateBlogMetadata({ params }: { params: { slug: string } }) {
+  const blog = await sanityFetch<BlogPostResponse>({
+    query: blogPostBySlugQuery,
+    params: { slug: params.slug },
+    tags: ['blogPost'],
+  });
+  if (!blog) return {};
+  const siteUrl = process.env.NEXT_PUBLIC_SITE_URL || 'http://localhost:3000';
+  const url = `${siteUrl}/blogs/${blog.slug}`;
+  const title = blog.seo?.metaTitle || blog.title;
+  const description = blog.seo?.metaDescription || blog.description;
+  const image = blog.thumbnail;
+  return {
+    title,
+    description,
+    alternates: { canonical: url },
+    openGraph: {
+      title,
+      description,
+      url,
+      type: 'article',
+      images: image ? [image] : undefined,
+    },
+    twitter: {
+      card: 'summary_large_image',
+      title,
+      description,
+      images: image ? [image] : undefined,
+    },
+  };
+}
