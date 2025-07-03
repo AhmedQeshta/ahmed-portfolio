2f478857eb703026e333878de7e3cfaa4883a138
Ahmed Qeshta <ahmedqeshta1999@gmail.com>
2025-07-03 21:05:09 +0300

    add  search component

--- components/Search.tsx
+++ components/Search.tsx
@@ -2,6 +2,7 @@
 
 import { useSearch } from '@/hooks/useSearch';
 import { XIcon } from 'lucide-react';
+import DefaultInput from '@/components/ui/DefaultInput';
 
 interface SearchInterface {
   action: string;
@@ -14,14 +15,13 @@ const Search = ({ action }: SearchInterface) => {
     <div className="mx-auto max-w-5xl px-4 py-5">
       <form onSubmit={handleSearch} className="flex gap-2">
         <div className="relative flex-1">
-          <input
-            placeholder="Search blogs..."
-            type="text"
+          <DefaultInput
             name="q"
+            type="text"
             value={query}
-            onChange={(e) => setQuery(e.target.value)}
-            className="w-full px-4 py-2 rounded-lg border border-gray-300 focus:outline-none pr-10"
-            autoFocus
+            handleInputChange={setQuery}
+            placeholder="Search blogs..."
+            autoFocus={true}
             autoComplete="off"
             autoCorrect="off"
             style={{ WebkitAppearance: 'none' }}
