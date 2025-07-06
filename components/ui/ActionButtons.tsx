91dd9500afb89e94b8123a06122d74e3c08ffb9d
Ahmed Qeshta <ahmedqeshta1999@gmail.com>
2025-07-06 11:58:50 +0300

    refactor ActionButtons component to use Link instead of button for navigation, enhancing accessibility and user experience

--- components/ui/ActionButtons.tsx
+++ components/ui/ActionButtons.tsx
@@ -1,6 +1,7 @@
 'use client';
 import { cn } from '@/utils/statusColor';
 import ScrollAnimation from './ScrollAnimation';
+import Link from 'next/link';
 
 interface listLinks {
   id?: number;
@@ -16,24 +17,26 @@ interface ActionButtonsProps {
 export default function ActionButtons({ listLinks }: ActionButtonsProps) {
   if (!listLinks) return null;
 
-  const handleButtonClick = (e: React.MouseEvent<HTMLButtonElement>, link?: string) => {
-    e.stopPropagation();
-    e.preventDefault();
-    if (link) {
-      window.open(link, '_blank', 'noopener,noreferrer');
-    }
-  };
+  // const handleButtonClick = (e: React.MouseEvent<HTMLButtonElement>, link?: string) => {
+  //   e.stopPropagation();
+  //   e.preventDefault();
+  //   if (link) {
+  //     window.open(link, '_blank', 'noopener,noreferrer');
+  //   }
+  // };
 
   return (
     <ScrollAnimation direction="down" delay={0.4} className="flex gap-4">
       {listLinks.map(({ id, text, link, customStyle, icon }) => (
-        <button
+        <Link
           key={id}
-          onClick={(e) => handleButtonClick(e, link)}
-          className={cn(customStyle, 'flex items-center gap-2')}>
+          href={link ?? '/'}
+          target="_blank"
+          rel="noopener noreferrer"
+          className={cn(customStyle, 'flex items-center gap-2 hover:text-blue-400')}>
           {icon && icon}
           {text}
-        </button>
+        </Link>
       ))}
     </ScrollAnimation>
   );
