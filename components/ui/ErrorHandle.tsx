82b2bb4b08a5c0ab662176576c24da3b46e04002
Ahmed Qeshta <ahmedqeshta1999@gmail.com>
2025-07-18 21:17:42 +0300

    update footer to be sticky on the bottom

--- components/ui/ErrorHandle.tsx
+++ components/ui/ErrorHandle.tsx
@@ -7,13 +7,6 @@ export default function ErrorHandle({ id, title, description }: IErrorHandle) {
   return (
     <section id={id} className="py-20" data-testid={id}>
       <div className="mx-auto max-w-5xl px-4" data-testid="error-container">
-        <motion.h2
-          className="text-3xl font-semibold mb-8 gradient-text"
-          initial={{ filter: 'blur(10px)', opacity: 0 }}
-          animate={{ filter: 'blur(0px)', opacity: 1 }}
-          transition={{ duration: 0.5 }}>
-          {title}
-        </motion.h2>
         <motion.div
           className="text-center text-red-400"
           initial={{ filter: 'blur(10px)', opacity: 0 }}
