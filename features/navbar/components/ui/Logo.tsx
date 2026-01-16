d7941c465da28f683bb761e86d9e041ae1baedba
Ahmed Qeshta <ahmedqeshta1999@gmail.com>
2026-01-16 15:52:38 +0200

    Refactor Logo component to use theme-based images and remove legacy logo
    - Updated Logo component to conditionally render light and dark logos based on the current theme.
    - Removed the old logo file and added new logo images for both light and dark themes.
    - Enhanced visual consistency by utilizing optimized images for better performance.

--- features/navbar/components/ui/Logo.tsx
+++ features/navbar/components/ui/Logo.tsx
@@ -2,19 +2,17 @@
 import Image from 'next/image';
 import OptimizedLink from '@/features/shard/components/ui/OptimizedLink';
 import { useThemeToggle } from '@/features/theme/hooks/useThemeToggle';
-import { Dot } from 'lucide-react';
+
+import logoLight from '@/public/logo-light.png';
+import logoDark from '@/public/logo-dark.png';
 
 export default function Logo() {
   const { isDark } = useThemeToggle();
-  const color = isDark ? '#eeaef7' : '#590d63';
-  const styleLogo = isDark
-    ? 'bg-gradient-to-r from-purple-400 via-pink-400 to-blue-400 bg-clip-text text-transparent'
-    : '#0f172af2';
   return (
     <OptimizedLink href="/" className="flex-shrink-0">
       <div className="relative hover:scale-105 transition-transform duration-200">
         <Image
-          src="/logo.png"
+          src={isDark ? logoLight : logoDark}
           alt="Ahmed Qeshta"
           width={60}
           height={20}
